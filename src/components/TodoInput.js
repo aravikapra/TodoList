36491ae75c39f800147636e9fd2345cf871ff564
aravikapra <aravikaprakusiya@gmail.com>
2022-06-23 16:40:37 +0700

    Create Todo List

---
+++ src/components/TodoInput.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+    const TodoInput = ({createTodoItem}) =>{
+        const [value,setValue] = React.useState('')
+
+        const handleSubmit = e =>{
+            e.preventDefault();
+            
+            if (value === ''){
+                return console.log("Please add something to do");
+            }
+            createTodoItem(value);
+            setValue('');
+        }
+        return (
+
+            <form onSubmit={handleSubmit}>
+                <input className="task-input" type="text" placeholder='create todo' value={value} onChange={(e) => setValue(e.target.value)}>
+
+                </input>
+                <button onClick={handleSubmit}>Create</button>
+            </form>
+            
+          )
+        }
+export default TodoInput
---
+++ src/components/TodoItem.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+    const TodoItem = ({item, index, deleteTodoItem, completeTodoItem, updateTodoItem }) =>{
+        
+        return (
+                <div className='todo-list'>
+                    <li>{item.todo}</li>
+                    <div className='btns'>
+                        <button onClick={() => completeTodoItem(index)}>Complete</button>
+                        <button onClick={() => updateTodoItem(index)}>Update</button>
+                        <button onClick={() => deleteTodoItem(index)}>X</button>
+                    </div>
+                </div>
+          )
+    }
+
+export default TodoItem
